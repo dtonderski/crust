03678712930a0bd2bf9dc6a9c77ce4179224d113
David Tonderski <dtonderski@gmail.com>
2026-05-03 18:52:31 +0200

    feat: match error handling and args with nlsandler
    https://github.com/nlsandler/writing-a-c-compiler-tests

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,36 @@ use std::{env, fs::File, io::Read};
 fn main() {
     let mut args = env::args();
     let program_name = args.next().unwrap_or_else(|| "crust".to_string());
-    let Some(file_path) = args.next() else {
-        eprintln!("usage: {program_name} <file>");
+
+    let mut file_path = None;
+    for arg in args {
+        match arg.as_str() {
+            "--lex" => {}
+            _ if arg.starts_with("--") => {
+                eprintln!("unsupported option: {arg}");
+                std::process::exit(1);
+            }
+            _ => file_path = Some(arg),
+        }
+    }
+
+    let Some(file_path) = file_path else {
+        eprintln!("usage: {program_name} [--lex] <file>");
         std::process::exit(1);
     };
 
-    let contents = read_file(&file_path).expect("failed to read input file");
+    let contents = match read_file(&file_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("failed to read input file: {err}");
+            std::process::exit(1);
+        }
+    };
 
-    let e = tokenize(&contents).expect("failed to tokenize");
-
-    dbg!(e);
+    if let Err(err) = tokenize(&contents) {
+        eprintln!("failed to tokenize: {err:?}");
+        std::process::exit(1);
+    }
 }
 
 fn read_file(file_path: &str) -> std::io::Result<String> {
